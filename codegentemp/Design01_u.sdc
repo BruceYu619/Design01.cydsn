eb36ff17d351d9257c8fa7b3720235b5650b88aa
BruceYu619 <yuyiyang_2000@hotmail.com>
2021-03-18 20:33:40 -0400

    Partie 2

--- codegentemp/Design01_u.sdc
+++ codegentemp/Design01_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\Bruce\Documents\PSoC Creator\Lab4\Design01.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\Bruce\Documents\PSoC Creator\Lab4\Design01.cydsn\Design01.cyprj
-# Date: Wed, 10 Mar 2021 21:59:56 GMT
+# Date: Thu, 18 Mar 2021 19:52:58 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -25,12 +25,46 @@ void vGreenTask()
         Cy_GPIO_Write(GREENLED_0_PORT,GREENLED_0_NUM,1);
     }
 }
+SemaphoreHandle_t bouton_semph;
+int i=0;
+void isr_bouton(void)
+{   
+    i++;
+    xSemaphoreGiveFromISR(bouton_semph,NULL);
+    Cy_GPIO_ClearInterrupt(BOUTON_0_PORT,BOUTON_0_NUM);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);    
+}
+void bouton_task(){
+    for(;;){
+        xSemaphoreTake(bouton_semph,portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(20));
+        if (i%2 !=0){
+        UART_PutString("Bouton appuye");
+        }
+        else{
+        UART_PutString("Bouton relache");
+        }
+        
+    } 
+    xSemaphoreGive(bouton_semph);
+}
+
+
 int main(void)
 {
+    bouton_semph = xSemaphoreCreateBinary();
     __enable_irq(); /* Enable global interrupts. */
     
+    UART_Start();
+    Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
+    NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+    
+
     xTaskCreate(vGreenTask,"green",80,NULL,3,NULL);
+    xTaskCreate(bouton_task,"Etat",80,NULL,3,NULL);
     vTaskStartScheduler();
+    
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
