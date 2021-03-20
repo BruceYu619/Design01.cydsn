41d6760f3b975d9c42ad92f152678a221a8e4e7b
BruceYu619 <yuyiyang_2000@hotmail.com>
2021-03-20 14:18:55 -0400

    Partie 3

--- main_cm4.c
+++ main_cm4.c
@@ -13,8 +13,9 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
-#include "params.h"
+//#include "params.h"
 #include "queue.h"
+#include "params.h"
 
 void vGreenTask()
 {
@@ -39,14 +40,30 @@ void bouton_task(){
         xSemaphoreTake(bouton_semph,portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(20));
         if (i%2 !=0){
-        UART_PutString("Bouton appuye");
+        UART_PutString("Bouton appuye \r\n");
         }
         else{
-        UART_PutString("Bouton relache");
-        }
-        
+        UART_PutString("Bouton relache \r\n");
+        }  
     } 
     xSemaphoreGive(bouton_semph);
+}
+task_params_t task_A = {
+    .delay = 1000,
+    .message = "Tache A en cours\n\r"
+};
+task_params_t task_B = {
+    .delay = 999,
+    .message = "Tache B en cours\n\r"
+};
+
+
+void print_loop(task_params_t*params){
+    
+    for(;;){
+    vTaskDelay(pdMS_TO_TICKS(params->delay));
+    UART_PutString(params->message);
+    }
 }
 
 
@@ -63,6 +80,8 @@ int main(void)
 
     xTaskCreate(vGreenTask,"green",80,NULL,3,NULL);
     xTaskCreate(bouton_task,"Etat",80,NULL,3,NULL);
+    xTaskCreate(print_loop, "task A", configMINIMAL_STACK_SIZE, (void *) &task_A, 1, NULL);
+    xTaskCreate(print_loop, "task B", configMINIMAL_STACK_SIZE, (void *) &task_B, 1, NULL);
     vTaskStartScheduler();
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
